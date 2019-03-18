1073e4f65c5a9e4b4aadc698e88c24fa40af9e3c
Vijaya Prakash <mvjp.183@gmail.com>
2019-03-18 16:29:25 +0530

    Update socialNetwork.h

--- socialNetwork.h
+++ socialNetwork.h
@@ -13,23 +13,15 @@ public:
 	socialNetwork& operator=(socialNetwork &&) = delete;
 	void addUser(string name);
 	void deleteUser(string name);
-<<<<<<< HEAD
+
 	shared_ptr<user> getUser(string name);
+	shared_ptr<user> getUser(unsigned long id);
 	set<unsigned long>searchUserByName(string name);
 	set<unsigned long>searchUserByAge(unsigned int age);
 	set<unsigned long>searchUserByHobbies(set<string> hobbies);
 	set<unsigned long>getFriendsOfUser(shared_ptr<user> fuser);
 	void addFriend(shared_ptr<user> ouser, shared_ptr<user> fuser);
-=======
-	user* getUser(string name);
-	user* getUser(unsigned long id);
-	set<unsigned long>searchUserByName(string name);
-	set<unsigned long>searchUserByAge(unsigned int age);
-	set<unsigned long>searchUserByHobbies(set<string> hobbies);
-	set<unsigned long>getFriendsOfUser(user *fuser);
-	set<user*>getFriendsOfUser(set<unsigned long>);
-	void addFriend(user *ouser, user *fuser);
->>>>>>> 88ac488b2a980bcd2e7fa7a851cee7e31278af1b
+
 	void printAllUsers();
 	void notify(user*);
 };
